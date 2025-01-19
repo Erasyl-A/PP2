bb33b9d42cb3dea195a5c5205dc6050c0a96ebfa
Erasyl-A <anuarbekoverasyl632@gmail.com>
2025-01-20 00:53:35 +0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,34 @@
+#python variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#variable names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Assign mltiple values
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#output variables
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+#global variables
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
