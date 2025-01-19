809dfaae627eb8d15bc2e83ff853868901d4b3a6
Erasyl-A <anuarbekoverasyl632@gmail.com>
2025-01-20 01:00:10 +0600

    Create strings.py

---
+++ strings/strings.py
@@ -0,0 +1,25 @@
+#python strings
+print("Hello")
+print('Hello')
+
+#slicing strings
+b = "Hello, World!"
+print(b[2:5])
+
+#modify strings
+a = "Hello, World!"
+print(a.upper())
+
+#concatenate strings
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#format strings
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+#escape characters
+txt = "We are the so-called \"Vikings\" from the north.
